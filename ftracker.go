a0cd6bddf75d7d22ec39562b945bdf3443edf7aa
bear-brown-beard <m.osipov2024@yandex.kz>
2024-09-12 15:03:44 +0500

    sixth iteration

--- ftracker.go
+++ ftracker.go
@@ -100,9 +100,9 @@ const (
 // duration float64 — длительность тренировки в часах.
 // weight float64 — вес пользователя.
 // height float64 — рост пользователя.
-func WalkingSpentCalories(action int, duration, weight, height float64) float64 {
+func WalkingSpentCalories(lengthPool, countPool, action int, duration, weight, height float64) float64 {
     // ваш код здесь
-    walkingCalories := ((walkingCaloriesWeightMultiplier * weight + (math.Pow(swimmingMeanSpeed(), 2.0) / height) * walkingSpeedHeightMultiplier * weight) * duration * minInH)
+    walkingCalories := ((walkingCaloriesWeightMultiplier * weight + (math.Pow(swimmingMeanSpeed(lengthPool, countPool, duration), 2.0) / height) * walkingSpeedHeightMultiplier * weight) * duration * minInH)
     return walkingCalories
 }
 
