7104a927c385bd1632bfeb5afe0e6839b184ea8f
bear-brown-beard <m.osipov2024@yandex.kz>
2024-09-12 16:28:49 +0500

    11th

--- ftracker.go
+++ ftracker.go
@@ -102,7 +102,8 @@ const (
 // height float64 — рост пользователя.
 func WalkingSpentCalories(action int, duration, weight, height float64) float64 {
     // ваш код здесь
-    walkingCalories := ((walkingCaloriesWeightMultiplier * weight + (math.Pow(meanSpeed(action, duration), 2.0) / (height / 100)) * walkingSpeedHeightMultiplier * weight) * duration * minInH)
+    speedKmch := meanSpeed(action, duration) / 3.6
+    walkingCalories := ((walkingCaloriesWeightMultiplier * weight + (math.Pow(speedKmch, 2.0) / (height / 100)) * walkingSpeedHeightMultiplier * weight) * duration * minInH)
     return walkingCalories
 }
 
